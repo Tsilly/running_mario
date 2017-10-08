dc1b9cfc78706ec7d8aeb3a8bb2f34b4499e9069
Jill  Li <jill.li87@gmail.com>
2017-10-08 12:07:35 +0200

    WIP: mario frozen

--- src/mario_run/core.clj
+++ src/mario_run/core.clj
@@ -5,24 +5,24 @@
 (defn setup []
   {:mario (load-image "resources/mario.png")
    :background (load-image "resources/background.jpg")
-   :x-param 10})
+   :y-param 10})
 
-(defn update [state]
-  (if (<= (:y-param state) 300)
-    (update-in state [:y-param] inc)))
+; (defn update [state]
+;   (if (<= (:y-param state) 300)
+;     (update-in state [:y-param] inc)))
 
 
-(defn draw [state]
-  (background-image (:background state))
-  (resize (:mario state) (* 36 2) (* 49 2))
-  (image (:mario state) 10 (:y-param state)))
+(defn draw []
+  (background-image (:background)))
+  ; (resize (:mario state) (* 36 2) (* 49 2))
+  ; (image (:mario state) 10 (:y-param state)))
 
 (defsketch example
   :title "Example"
   :setup setup
-  :update update
+  ; :update update
   :draw draw
   :middleware [m/fun-mode]
-  :size [1680 1050])
+  :size [800 499])
 
 (defn -main [& args])
